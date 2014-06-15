3528fa9ce22a7907725dce9eb7075959e63d029b
iwanovich <Iwanovich@gmail.com>
2014-06-15 15:48:30 +0000

    created modifier to remove tokens from html file(names)

---
+++ validator/src/main/java/com/flameling/uva/thesis/validator/Constants.java
@@ -0,0 +1,8 @@
+package com.flameling.uva.thesis.validator;
+
+import java.io.File;
+
+public class Constants {
+	static public final File OUTPUT_FOLDER = new File("htmloutput");
+	static public final String TOKEN_KEY = "com.flameling.SACSRF.CSRF_NONCE";
+}
