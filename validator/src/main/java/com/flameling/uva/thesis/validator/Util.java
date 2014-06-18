ddf73c1271352a81978d5f6280f2d7edb0b75afa
iwanovich <Iwanovich@gmail.com>
2014-06-18 16:44:58 +0000

    FileNameModifier is not needed anymore, since the token get stripped from the url in an earlier stage. Created first (successful) attempt in cleanly parsing the DOM.

--- validator/src/main/java/com/flameling/uva/thesis/validator/Util.java
+++ validator/src/main/java/com/flameling/uva/thesis/validator/Util.java
@@ -1,5 +1,11 @@
 package com.flameling.uva.thesis.validator;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -31,6 +37,55 @@ public class Util {
 		}
 		
 		return result;
+	}
+	
+	public static List<File> getFiles(File folder, boolean recursive){
+		if(recursive)
+			return getFilesRecursively(folder);
+		else
+			return getFiles(folder);
+	}
+	
+	private static List<File> getFilesRecursively(File folder){
+		List<File> allFiles = new ArrayList<File>();
+		for(File file : folder.listFiles()){
+			if(file.isDirectory()){
+				allFiles.addAll(getFilesRecursively(file));
+			}
+			else if(file.isFile()) {
+				allFiles.add(file);
+			}
+		}
+		return allFiles;
+	}
+	
+	public static List<File> getDirectories(File folder, boolean recursive){
+		List<File> dirs = new ArrayList<File>();
+		for(File file : folder.listFiles(new FileFilter() {
+			
+			public boolean accept(File pathname) {
+				if (pathname.isDirectory())
+					return true;
+					else return false;
+			}
+		})){
+			dirs.add(file);
+			if(recursive)
+				dirs.addAll(getDirectories(file, recursive));
+		}
+		return dirs;
+	}
+	
+	private static List<File> getFiles(File folder){
+		File[] fileArray = folder.listFiles(new FileFilter() {
+			
+			public boolean accept(File pathname) {
+				if (pathname.isFile())
+					return true;
+					else return false;
+			}
+		});
+		return new ArrayList<File>(Arrays.asList(fileArray));
 	}
 	
 }
